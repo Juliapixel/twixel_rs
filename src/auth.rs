c412b43f9dfede2c9fd03b6e41ac47cd9dfc3bf0
Julia Borges <89038897+Juliapixel@users.noreply.github.com>
2024-01-06 00:22:32 -0300

    make sure Auth doesn't leak tokens on Debug impl

--- src/auth.rs
+++ src/auth.rs
@@ -1,8 +1,10 @@
+use std::fmt::Debug;
+
 use rand::Rng;
 
 use crate::irc_message::{owned::OwnedIrcMessage, command::IrcCommand};
 
-#[derive(Debug, Default, Clone)]
+#[derive(Default, Clone)]
 pub enum Auth {
     OAuth{ username: String, token: String },
     #[default]
@@ -18,13 +20,13 @@ impl Auth {
                         tags: None,
                         prefix: None,
                         command: IrcCommand::Pass,
-                        params: vec![String::from(format!("PASS {token}"))],
+                        params: vec![String::from(format!("{token}"))],
                     },
                     OwnedIrcMessage {
                         tags: None,
                         prefix: None,
                         command: IrcCommand::Nick,
-                        params: vec![String::from(format!("NICK {username}"))],
+                        params: vec![String::from(format!("{username}"))],
                     },
                 )
             },
@@ -35,16 +37,25 @@ impl Auth {
                         tags: None,
                         prefix: None,
                         command: IrcCommand::Pass,
-                        params: vec![String::from("PASS POGGERS")],
+                        params: vec![String::from("POGGERS")],
                     },
                     OwnedIrcMessage {
                         tags: None,
                         prefix: None,
                         command: IrcCommand::Nick,
-                        params: vec![String::from(format!("NICK justinfan{}", rng.gen_range(1..99999)))],
+                        params: vec![String::from(format!("justinfan{}", rng.gen_range(1..99999)))],
                     },
                 )
             }
+        }
+    }
+}
+
+impl Debug for Auth {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::OAuth { username, token: _ } => f.debug_struct("OAuth").field("username", username).field("token", &"*redacted for privacy*").finish(),
+            Self::Anonymous => write!(f, "Anonymous"),
         }
     }
 }
